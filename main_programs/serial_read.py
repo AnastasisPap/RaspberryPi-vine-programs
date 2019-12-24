feb4d03baab764f91cf970c4ae43d63770627f53
Anastasispap <anastasis.papapanagiotou@gmail.com>
2019-12-24 15:38:15 +0200

    Added frequency types

--- main_programs/serial_read.py
+++ main_programs/serial_read.py
@@ -5,7 +5,10 @@ atm_humidity = [0]
 soil_humidity = [0]
 air_quality = [0]
 plant_height = [0]
+freq_type = [0]
 ser = serial.Serial('/dev/ttyACM0', 9600)
+
+#16.30?57.20?100.00%?40?190.00?None
 
 def read_serial():
     i = 0
@@ -15,7 +18,8 @@ def read_serial():
             items = line.split("?")
             temperature[0] = items[0]
             atm_humidity[0] = items[1]
-            soil_humidity[0] = items[2]
+            soil_humidity[0] = items[2].split(".")[0]
             air_quality[0] = items[3]
             plant_height[0] = items[4].split(".")[0]
+            freq_type[0] = items[5]
             i = i+1
--- main_programs/write_to_json.py
+++ main_programs/write_to_json.py
@@ -4,14 +4,15 @@ import os
 dir = os.getcwd()
 
 
-def write_to_json(pi_temp, temp, humidity, soil_hum, air_q, plant_h):
+def write_to_json(pi_temp, temp, humidity, soil_hum, air_q, plant_h, freq):
     data = {
         "pi_temp": pi_temp,
         "temp": temp,
         "humidity": humidity,
         "soil_hum": soil_hum,
         "air_q": air_q,
-        "plant_h": plant_h
+        "plant_h": plant_h,
+        "freq_type": freq
     }
     file_name = "info.json"
     with open(file_name, 'w') as file:
